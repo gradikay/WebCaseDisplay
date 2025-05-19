448d530bc13097c9acfd29e367f69a8130aae57f
GRADI <56418645+gradikay@users.noreply.github.com>
2025-05-19 21:21:17 +0000

    add jamaican website

--- assets/js/data.js
+++ assets/js/data.js
@@ -15,7 +15,7 @@ const SHOWCASE_WEBSITES = [
             "created": "May 10, 2025",
             "technologies": ["HTML", "CSS", "JavaScript"],
             "organization": "Artivico",
-            "type": "Restaurant"
+            "type": "Jamaican Restaurant"
         }
     },
     {
@@ -33,18 +33,15 @@ const SHOWCASE_WEBSITES = [
     },
     {
         "id": 3,
-        "title": "CSS-Tricks",
-        "description": "Tips, tricks, and techniques for web developers",
-        "url": "https://css-tricks.com/",
-        "thumbnail": "assets/images/thumbnails/css-tricks.svg",
+        "title": "Island Flavors",
+        "description": "Complete Restaurant Website Template with Jamaican/Caribbean Theme",
+        "url": "https://gradikay.github.io/JamaicanCuisine/",
+        "thumbnail": "assets/images/IslandFlavors-JamaicanCuisine.webp",
         "metadata": {
             "created": "May 10, 2025",
-            "technologies": ["WordPress", "CSS", "HTML", "JavaScript", "SVG"],
-            "organization": "DigitalOcean",
-            "type": "Web Development Blog",
-            "monthly_visitors": "5 million+",
-            "articles": "2,000+",
-            "focus": "Frontend Development"
+            "technologies": ["HTML", "CSS", "JavaScript"],
+            "organization": "Artivico",
+            "type": "Jamaican Restaurant"
         }
     },
     {
